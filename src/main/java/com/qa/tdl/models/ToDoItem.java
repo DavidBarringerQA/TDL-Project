eddc52c991c27f19ddc97a21e03f2470b06b1320
DavidBarringerQA <DBarringer@qa.com>
2021-08-04 17:14:35 +0100

    TDL-15 #comment Integration tests for controller

--- src/main/java/com/qa/tdl/models/ToDoItem.java
+++ src/main/java/com/qa/tdl/models/ToDoItem.java
@@ -44,46 +44,15 @@ public class ToDoItem{
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (completed ? 1231 : 1237);
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + priority;
-		result = prime * result + ((time == null) ? 0 : time.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
+	  if(!(obj instanceof ToDoItem)){
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ToDoItem other = (ToDoItem) obj;
-		if (completed != other.completed)
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (priority != other.priority)
-			return false;
-		if (time == null) {
-			if (other.time != null)
-				return false;
-		} else if (!time.equals(other.time))
-			return false;
-		return true;
+		}
+		ToDoItem other = (ToDoItem)obj;
+		if(id == 0L && other.id == 0L) {
+			return super.equals(obj);
+		}
+		return id == other.id;
 	}
 
 	public Long getId() {
